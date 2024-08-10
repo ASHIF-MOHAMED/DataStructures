058f1627111da209afd973f90c7807879193790e
ASHIF-MOHAMED <ashifmohd.offl@gmail.com>
2024-08-10 13:13:23 +0530

    Create SingleLinkedList.cpp

---
+++ SingleLinkedList.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class node
+{
+int data;
+node*next;
+node(int data)
+{
+data=data;
+next=nullptr;
+}
+int main()
+{
+node*head=NULL;
+node*mid=NULL;
+node*tail=NULL;
+head=new node(1);
+mid=new node(2);
+tail=new node(3);
+head->next=mid;
+mid->next=tail;
+tail->next=NULL;
+}
