c68a857ffe903eb1c2e82e67f25f2560cb8ca12c
ASHIF-MOHAMED <ashifmohd.offl@gmail.com>
2024-08-11 11:06:58 +0530

    Create RemoveDuplicates.cpp
    C++ Program to remove duplicates from a sorted linked list. Write a function that takes a list sorted in non- decreasing order and deletes any duplicate nodes from the list. The list should only be traversed once.

---
+++ RemoveDuplicates.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node* next;
+};
+void removeduplicates(node* head)
+{
+    node* temp=head;
+    while(temp->next!=NULL)
+    {
+        if(temp->data==temp->next->data)
+        {
+            temp->next=temp->next->next;
+        }
+        else
+        {
+            temp=temp->next;
+        }
+    }
+}
+void display(node* n)
+{
+    node* var=n;
+    while(var!=NULL)
+    {
+        cout<<var->data<<" ";
+        var=var->next;
+    }
+
+}
+int main()
+{
+    node* head=NULL;
+    node* second=NULL;
+    node* third=NULL;
+    node* four=NULL;
+    node* five=NULL;
+    node* six=NULL;
+
+    head=new node();
+    second=new node();
+    third=new node();
+    four=new node();
+    five=new node();
+    six=new node();
+
+    head->data=11;
+    head->next=second;
+    second->data=11;
+    second->next=third;
+    third->data=11;
+    third->next=four;
+    four->data=21;
+    four->next=five;
+    five->data=43;
+    five->next=six;
+    six->data=60;
+    six->next=NULL;
+    removeduplicates(head);
+    display(head);
+    return 0;
+}
